024c9b84a49220b4fef43a1cdaf735c53e596baf
Balu <balu.triassic@gmail.com>
2018-05-09 18:02:47 +0530

    First Commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Recipe } from './../recipe.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+recipes: Recipe[] = [new Recipe('The Real Deal With Hunan Cuisine',
+'Chairman Mao Zedong, who hailed from spice-loving Hunan province, once said, "You can\'t be a revolutionary if you don\'t eat chilies."' +
+' The Hunanese food writer Liu Guochu says Mao loved spice so much he sprinkled ground chilies on slices of watermelon.',
+'https://www.seriouseats.com/images/2015/01/20150130-hunan-manor-beef-max-falkowitz.jpg')];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,16 @@
+import { Ingredient } from './../shared/ingredient.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
