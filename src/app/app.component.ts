8c5b617cc3040c0d10af83bcb879d0e463af9654
Balu <balu.triassic@gmail.com>
2018-05-14 15:33:23 +0530

    Last Chck In

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '';
+  loadedFeatures = 'recipe';
+  onNavigate(feature: string) {
+    this.loadedFeatures = feature;
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,13 @@ export class RecipeListComponent implements OnInit {
 recipes: Recipe[] = [new Recipe('The Real Deal With Hunan Cuisine',
 'Chairman Mao Zedong, who hailed from spice-loving Hunan province, once said, "You can\'t be a revolutionary if you don\'t eat chilies."' +
 ' The Hunanese food writer Liu Guochu says Mao loved spice so much he sprinkled ground chilies on slices of watermelon.',
-'https://www.seriouseats.com/images/2015/01/20150130-hunan-manor-beef-max-falkowitz.jpg')];
+'https://www.seriouseats.com/images/2015/01/20150130-hunan-manor-beef-max-falkowitz.jpg'),
+new Recipe('The Real Deal With Hunan Cuisine',
+'Chairman Mao Zedong, who hailed from spice-loving Hunan province, once said, "You can\'t be a revolutionary if you don\'t eat chilies."' +
+' The Hunanese food writer Liu Guochu says Mao loved spice so much he sprinkled ground chilies on slices of watermelon.',
+'https://www.seriouseats.com/images/2015/01/20150130-hunan-manor-beef-max-falkowitz.jpg')
+
+];
   constructor() { }
 
   ngOnInit() {
